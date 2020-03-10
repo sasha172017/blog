1d1418fa5cfb164c595b6ce5d232d4defa811725
Sasha Nosenko <noi95noi@gmail.com>
2020-03-10 20:31:40 +0200

    generate thumb for post and user

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
